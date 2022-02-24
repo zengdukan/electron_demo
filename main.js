8d07f83c0399563b689e6acbbc7aafb8fd36f407
zengdukan <zengdukan@163.com>
2022-02-24 16:57:45 +0800

    menu and key

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // main.js
 
 // 控制应用生命周期和创建原生浏览器窗口的模组
-const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
+const { app, BrowserWindow, ipcMain, nativeTheme, Menu, MenuItem, globalShortcut } = require('electron')
 const path = require('path')
 
 function initIpcMain() {
@@ -37,12 +37,26 @@ function createWindow () {
   // mainWindow.webContents.openDevTools()
 
   initIpcMain();
+
+  const menu = new Menu();
+  menu.append(new MenuItem({
+    label: 'Electron',
+    submenu: [{
+        role: 'help',
+        accelerator: process.platform === 'darwin' ? 'Alt+Cmd+I' : 'Alt+Shift+I',
+        click: () => { console.log('Electron rocks!'); }
+    }]
+  }));
+  Menu.setApplicationMenu(menu);
 }
 
 // 这段程序将会在 Electron 结束初始化
 // 和创建浏览器窗口的时候调用
 // 部分 API 在 ready 事件触发后才能使用。
 app.whenReady().then(() => {
+  globalShortcut.register('Alt+CommandOrControl+I', () => {
+    console.log('Electron loves global shortcuts!')
+  });
   createWindow()
 
   app.on('activate', function () {
